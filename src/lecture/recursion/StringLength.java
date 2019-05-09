b920c1b84d522f7d97c9f3f978f7dc325e8225e6
pch8388 <pch838811@gmail.com>
2019-05-10 00:15:16 +0900

    recursion #4

--- src/lecture/recursion/StringLength.java
+++ src/lecture/recursion/StringLength.java
@@ -8,7 +8,26 @@ public class StringLength {
         return 1 + length(str.substring(1));
     }
 
+    private static void printChars(String str) {
+        if ("".equals(str)) {
+            return;
+        }
+        System.out.print(str.charAt(0));
+        printChars(str.substring(1));
+    }
+
+    private static void printCharsReverse(String str) {
+        if ("".equals(str)) {
+            return;
+        }
+        printCharsReverse(str.substring(1));
+        System.out.print(str.charAt(0));
+    }
+
     public static void main(String[] args) {
         System.out.println(length("hello world"));
+        printChars("print characters");
+        System.out.println();
+        printCharsReverse("print characters");
     }
 }
