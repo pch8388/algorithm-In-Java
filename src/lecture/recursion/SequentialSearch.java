4588a29363d2e374f5efcd30aa6a7b6a56f86c9e
pch8388 <pch838811@gmail.com>
2019-05-11 22:54:38 +0900

    recursion #5

---
+++ src/lecture/recursion/SequentialSearch.java
@@ -0,0 +1,24 @@
+package lecture.recursion;
+
+public class SequentialSearch {
+    private static int search(int[] data, int n, int target) {
+        for (int i = 0; i < n; i++) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int recursionSearch(int[] data, int begin, int end, int target) {
+        if (begin > end) return -1;
+        else if (target == data[begin]) return begin;
+        else return recursionSearch(data, begin+1, end, target);
+    }
+
+    public static void main(String[] args) {
+        int[] data = {2, 4, 7, 6, 3};
+        System.out.println(search(data, 5, 3));
+        System.out.println(recursionSearch(data, 0, 5, 3));
+    }
+}
